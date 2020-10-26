20dea18fdbaef2a409f341d19a379de572a8d561
mkovachev <mkovachev6@gmail.com>
2020-10-26 19:20:21 +0200

    refactor

--- Pazaar.Domain/Model/Ad/Ad.cs
+++ Pazaar.Domain/Model/Ad/Ad.cs
@@ -5,23 +5,49 @@ namespace Pazaar.Domain.Model.Ad
 {
     public class Ad : AuditEntity
     {
-        internal Ad(IReadOnlyCollection<Category> categories, string titel, Gallery gallery, string description, decimal price, bool isActive)
+        internal Ad(IReadOnlyCollection<Category> categories, string title, Gallery gallery, string description, decimal price, bool isActive)
         {
             this.Categories = categories;
-            this.Titel = titel;
+            this.Title = title;
             this.Gallery = gallery;
             this.Description = description;
             this.Price = price;
             this.IsActive = isActive;
         }
-        public string Titel { get; private set; }
+        public string Title { get; private set; }
         public IReadOnlyCollection<Category> Categories { get; private set; } = new List<Category>();
         public Gallery Gallery { get; private set; }
         public string Description { get; private set; }
         public decimal Price { get; private set; }
         public bool IsActive { get; private set; }
 
+        private Ad UpdateTitle(string title)
+        {
+            this.Title = title;
 
+            return this;
+        }
+
+        private Ad UpdateDescription(string description)
+        {
+            this.Description = description;
+
+            return this;
+        }
+
+        private Ad UpdatePrice(decimal price)
+        {
+            this.Price = price;
+
+            return this;
+        }
+
+        private Ad ChangeIsActive()
+        {
+            this.IsActive = !this.IsActive;
+
+            return this;
+        }
 
     }
 }
