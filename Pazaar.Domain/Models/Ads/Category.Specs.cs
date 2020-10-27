04dad4ba8c477b14251bfb1b3304b5309a12d335
mkovachev <mkovachev6@gmail.com>
2020-10-27 11:36:34 +0200

    fully tested

---
+++ Pazaar.Domain/Models/Ads/Category.Specs.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using System;
+using Xunit;
+
+namespace Pazaar.Domain.Models.Ads
+{
+    public class CategorySpecs
+    {
+        [Fact]
+        public void CategoryWithoutName_Should_Throw_ArgumentNullException()
+        {
+            // Act
+            Action act = () => new Category("");
+
+            // Assert
+            act.Should().Throw<ArgumentNullException>().WithMessage("Please add category name");
+        }
+
+        [Fact]
+        public void InvalidCategoryNameLenght_Should_Throw_Exception()
+        {
+            // Act
+            Action act = () => new Category("");
+
+            // Assert
+            act.Should().Throw<ArgumentException>()
+                .WithMessage($"Name must be between 5 and 30 characters");
+        }
+    }
+}
