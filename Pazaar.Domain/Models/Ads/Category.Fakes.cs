1b8831a2315f607511509e2bb4827cc37082011b
mkovachev <mkovachev6@gmail.com>
2020-10-27 11:40:29 +0200

    fully implemented

--- Pazaar.Domain/Models/Ads/Category.Fakes.cs
+++ Pazaar.Domain/Models/Ads/Category.Fakes.cs
@@ -1,7 +1,20 @@
-﻿namespace Pazaar.Domain.Models.Ads
+﻿using FakeItEasy;
+using System;
+
+namespace Pazaar.Domain.Models.Ads
 {
     public class CategoryFakes
     {
+        public const string ValidCategoryName = "Cars";
 
+        public class CategoryDummyFactory : IDummyFactory
+        {
+            public bool CanCreate(Type type) => type == typeof(Category);
+
+            public object? Create(Type type)
+                => new Category(ValidCategoryName);
+
+            public Priority Priority => Priority.Default;
+        }
     }
 }
