496cab9c09bf01b31adcfbcdb39e55a127d495f4
mkovachev <mkovachev6@gmail.com>
2020-11-03 14:22:00 +0200

    refactor

---
+++ Pazaar.Domain/Common/Guard.cs
@@ -0,0 +1,78 @@
+﻿using Pazaar.Domain.Exceptions;
+using Pazaar.Domain.Models;
+using System;
+
+namespace Pazaar.Domain.Common
+{
+    public static class Guard
+    {
+        public static void AgainstEmptyString<TException>(string value)
+            where TException : BaseDomainException, new()
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                ThrowException<TException>($"{value} cannot be null ot empty.");
+            }
+        }
+
+        public static void ForStringLength<TException>(string value, int minLength, int maxLength)
+            where TException : BaseDomainException, new()
+        {
+            AgainstEmptyString<TException>(value);
+
+            if (minLength > value.Length || value.Length > maxLength)
+            {
+                ThrowException<TException>($"{value} must have between {minLength} and {maxLength} symbols.");
+            }
+        }
+
+        public static void AgainstOutOfRange<TException>(int number, int min, int max, string name = "Value")
+            where TException : BaseDomainException, new()
+        {
+            if (min > number || number > max)
+            {
+                ThrowException<TException>($"{name} must be between {min} and {max}.");
+            }
+        }
+
+        public static void AgainstOutOfRange<TException>(decimal number, decimal min, decimal max)
+            where TException : BaseDomainException, new()
+        {
+            if (min > number || number > max)
+            {
+                ThrowException<TException>($"{number} must be between {min} and {max}.");
+            }
+        }
+
+        public static void ForValidUrl<TException>(string url)
+            where TException : BaseDomainException, new()
+        {
+            if (url.Length > ModelConstants.Image.ImageUrlMaxLength ||
+                            !Uri.IsWellFormedUriString(url, UriKind.Absolute))
+            {
+                ThrowException<TException>($"{url} must be a valid URL.");
+            }
+        }
+
+
+        public static void Against<TException>(object actualValue, object unexpectedValue)
+            where TException : BaseDomainException, new()
+        {
+            if (actualValue.Equals(unexpectedValue))
+            {
+                ThrowException<TException>($"{actualValue} must not be {unexpectedValue}.");
+            }
+        }
+
+        private static void ThrowException<TException>(string message)
+            where TException : BaseDomainException, new()
+        {
+            var exception = new TException
+            {
+                Error = message
+            };
+
+            throw exception;
+        }
+    }
+}
--- Pazaar.Domain/Models/Customer/Customer.cs
+++ Pazaar.Domain/Models/Customer/Customer.cs
@@ -1,96 +1,88 @@
 ﻿using Pazaar.Domain.Common;
+using Pazaar.Domain.Exceptions;
+using Pazaar.Domain.Models;
 using Pazaar.Domain.Models.Ads;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace Pazaar.Domain.Model.Customer
 {
+    using static ModelConstants.Customer;
     public class Customer : AuditableEntity, IAggregateRoot
     {
         private readonly HashSet<Ad> ads;
 
-        internal Customer(string email)
+        internal Customer(string name)
         {
-            this.Name = email;
-            this.Email = email;
+            this.ValidateName(name);
 
-            this.ads = new HashSet<Ad>();
-        }
-
-        internal Customer(string name, string email, string phoneNumber, string city, string profileImage)
-        {
             this.Name = name;
-            this.Email = email;
-            this.PhoneNumber = phoneNumber;
-            this.ProfileImage = profileImage;
-            this.City = city;
-
+            this.PhoneNumber = default!;
+            this.ProfileImage = default!;
+            this.City = default!;
             this.ads = new HashSet<Ad>();
         }
-
 
         public int Id { get; private set; }
         public string Name { get; private set; }
-        public string Email { get; private set; }
-        public string? PhoneNumber { get; private set; } = default!;
-        public string? City { get; private set; } = default!;
-        public string? ProfileImage { get; private set; } = default!;
+        public string PhoneNumber { get; private set; }
+        public string City { get; private set; }
+        public string ProfileImage { get; private set; }
         public IReadOnlyCollection<Ad> Ads => this.ads.ToList().AsReadOnly();
+
+        public void AddAd(Ad ad) => this.ads.Add(ad);
+        public void DeleteAd(Ad ad)
+        {
+            if (this.ads.Any(a => a.Title == ad.Title))
+            {
+                this.ads.Remove(ad);
+            }
+        }
 
         public Customer UpdateName(string name)
         {
+            this.ValidateName(name);
             this.Name = name;
 
             return this;
         }
-
-        public Customer UpdateEmail(string email)
-        {
-            this.Email = email;
-
-            return this;
-        }
-
         public Customer UpdatePhoneNumber(string phoneNumber)
         {
+            ValidatePhoneNumber(phoneNumber);
             this.PhoneNumber = phoneNumber;
 
             return this;
         }
         public Customer UpdateCity(string city)
         {
+            this.ValidateCity(city);
             this.City = city;
 
             return this;
         }
-
         public Customer UpdateProfileImage(string profileImage)
         {
+            ValidateProfileImage(profileImage);
             this.ProfileImage = profileImage;
 
             return this;
         }
 
-        public void AddAd(Ad ad)
+        private void ValidateName(string name)
         {
-            var adTitle = ad.Title;
-
-            if (this.ads.Any(ad => ad.Title == adTitle))
-            {
-                return;
-            }
-
-            this.ads.Add(ad);
+            Guard.ForStringLength<InvalidCustomerException>(name, NameMinLength, NameMaxLength);
         }
-
-        public void DeleteCategory(Ad ad)
+        private void ValidatePhoneNumber(string phoneNumber)
         {
-            var adTitle = ad.Title;
-
-            if (this.ads.Any(ad => ad.Title == adTitle))
-            {
-                this.ads.Remove(ad);
-            }
+            Guard.ForStringLength<InvalidCustomerException>(phoneNumber, PhoneNumberMinLength, PhoneNumberMaxLength);
+        }
+        private void ValidateCity(string city)
+        {
+            Guard.ForStringLength<InvalidCustomerException>(city, CityMinLength, CityMaxLength);
+        }
+        private void ValidateProfileImage(string profileImage)
+        {
+            Guard.ForValidUrl<InvalidCustomerException>(profileImage);
         }
     }
 }
